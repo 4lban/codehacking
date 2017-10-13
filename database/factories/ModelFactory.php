546e9b77a8181197159e93d35d0c38689b8c831e
Alban Maxhuni <alban.maxhuni@gmail.com>
2017-10-13 04:43:18 +0200

    test

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -14,8 +14,28 @@
 $factory->define(App\User::class, function (Faker\Generator $faker) {
     return [
         'name' => $faker->name,
+        'role_id' => $faker->numberBetween($min = 1, $max = 3),
+        'is_active' => $faker->numberBetween($min = 0, $max = 1),
         'email' => $faker->safeEmail,
         'password' => bcrypt(str_random(10)),
         'remember_token' => str_random(10),
     ];
 });
+
+$factory->define(App\Post::class, function (Faker\Generator $faker) {
+    return [
+        'user_id' => $faker->randomElement([12,13,14,15,16,17]),
+        'category_id' => $faker->randomElement([1,2,3,4,5,10,11]),
+        'photo_id' => $faker->randomElement([29,30,31,32,35,36]),
+        'title' => $faker->sentence(7, 11),
+        'body' => $faker->paragraph(rand(10,15), true),
+        'slug' => $faker->slug()
+    ];
+});
+
+$factory->define(App\Photo::class, function (Faker\Generator $faker) {
+    return [
+        'file' => 'placeholder.jpg'
+    ];
+});
+
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,28 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+//         $this->call(UsersTableSeeder::class);
+
+        /*
+         * if 0 we dont get errors when relationships exists
+         */
+        DB::statement('SET FOREIGN_KEY_CHECKS=0');
+
+        /* truncate means empty table */
+//        DB::table('users')->truncate();
+//        DB::table('posts')->truncate();
+
+        /*
+        Add 10 fake users with relationships posts
+        */
+        factory(App\User::class, 10)->create()->each(function($user){
+            $user->posts()->save(factory(App\Post::class)->make());
+        });
+
+        factory(App\Photo::class, 1)->create();
+        /*
+         * Add 10 posts
+         */
+//        factory(App\Post::class, 10)->create();
     }
 }
